43028178e8991c8a6c1481bd73a831bf7e135db6
yswi1892 <38318402+yswi1892@users.noreply.github.com>
2021-06-05 07:12:56 +0200

    Update application.py

--- application.py
+++ application.py
@@ -2,18 +2,18 @@ from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def main_page():
     
-#     return render_template("import.html")
+    return render_template("page.html")
 
-    if request.method == 'GET':
-        text = "ここに結果が出力されます"
-        return render_template("page.html",text=text)
-    elif request.method == 'POST':
-        name = request.form["name"]
-        text = "こんにちは" + name + "さん"
-        return render_template("page.html",text=text)
+#     if request.method == 'GET':
+#         text = "ここに結果が出力されます"
+#         return render_template("page.html",text=text)
+#     elif request.method == 'POST':
+#         name = request.form["name"]
+#         text = "こんにちは" + name + "さん"
+#         return render_template("page.html",text=text)
 
 ## 実行
 if __name__ == "__main__":
