8f29182411af59122209c3b9f8b1018789346098
yswi1892 <38318402+yswi1892@users.noreply.github.com>
2021-06-05 07:08:12 +0200

    Update application.py

--- application.py
+++ application.py
@@ -5,15 +5,15 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def main_page():
     
-    return render_template("import.html")
+#     return render_template("import.html")
 
-#     if request.method == 'GET':
-#         text = "ここに結果が出力されます"
-#         return render_template("page.html",text=text)
-#     elif request.method == 'POST':
-#         name = request.form["name"]
-#         text = "こんにちは" + name + "さん"
-#         return render_template("page.html",text=text)
+    if request.method == 'GET':
+        text = "ここに結果が出力されます"
+        return render_template("page.html",text=text)
+    elif request.method == 'POST':
+        name = request.form["name"]
+        text = "こんにちは" + name + "さん"
+        return render_template("page.html",text=text)
 
 ## 実行
 if __name__ == "__main__":
